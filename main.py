61ef7dbf28447d9d965ec337801e132e1709154c
tushar nitave <tushar nitave>
2017-09-27 21:16:29 +0530

    added gameover method and directions

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ else:
 # 2. Playing surface are for game
 
 resolution = (800,600)
-display_size = pygame.display.set_mode(resolution)
+playArea = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Snake Game')
 
 
@@ -38,3 +38,37 @@ snakeLength = [[100,50],[90,50],[80,50]]
 foodPosition = [random.randrange(1,80)*10, random.randrange(1,60)*10]
 foodSpawn = True
 
+
+#5. Method Game Over
+
+def gameOver():
+    disFont = pygame.font.SysFont('Monaco', 68)
+    surface = disFont.render('Game Over', True, white)
+    rect = surface.get_rect()
+    rect.midtop = (400,15)
+    playArea.blit(surface, rect)
+    pygame.display.flip()
+    time.sleep(4)
+    pygame.quit()   #termination pygame module
+    sys.exit()  #terminating python console
+
+#gameOver()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == odr('d'):
+                change_dir_to = 'RIGHT'
+            if event.key == pygame.K_LEFT or event.key == odr('a'):
+                change_dir_to = 'LEFT'
+            if event.key == pygame.K_UP or event.key == odr('w'):
+                change_dir_to = 'UP'
+            if event.key == pygame.K_DOWN or event.key == odr('s'):
+                change_dir_to = 'DOWN'
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.EVENT(pygame.QUIT))
+                sys.exit()
+                
