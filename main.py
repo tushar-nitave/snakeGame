c2b29a23b8a4b547ed9b29a9858e495801bb410c
tushar nitave <tushar nitave>
2017-09-27 22:42:54 +0530

    Finally ready with few fixes yet to come

--- main.py
+++ main.py
@@ -26,33 +26,47 @@ pygame.display.set_caption('Snake Game')
 red = pygame.Color(255, 255, 0) #Game over
 white = pygame.Color(255,255,255) #background
 black = pygame.Color(0,0,0) #score
+green = pygame.Color(0,0,255) #snake's body
+
+fpsController = pygame.time.Clock()
 
 #4. Variables for game play
 
-direction = 'RIGHT'
+direction = 'DOWN'
 change_dir_to = direction
-
 snakePosition = [700,100]
 snakeLength = [[100,50],[90,50],[80,50]]
 
 foodPosition = [random.randrange(1,80)*10, random.randrange(1,60)*10]
 foodSpawn = True
 
+score = 0
+
+speed = 15
 
 #5. Method Game Over
 
 def gameOver():
     disFont = pygame.font.SysFont('Monaco', 68)
-    surface = disFont.render('Game Over', True, white)
+    surface = disFont.render('Game Over', True, black)
     rect = surface.get_rect()
     rect.midtop = (400,15)
     playArea.blit(surface, rect)
+    showScore(0)
     pygame.display.flip()
-    time.sleep(4)
+    time.sleep(3)
     pygame.quit()   #termination pygame module
     sys.exit()  #terminating python console
 
-#gameOver()
+def showScore(flag = 1):
+    sFont = pygame.font.SysFont('Calibri', 20)
+    sSurface = sFont.render('Score: {0}'.format(score), True, black)
+    srect = sSurface.get_rect()
+    if flag == 1:
+        srect.midtop = (100,10)
+    else:
+        srect.midtop = (410, 100)
+    playArea.blit(sSurface, srect)
 
 while True:
     for event in pygame.event.get():
@@ -84,14 +98,25 @@ while True:
         direction = 'DOWN'
         
     #changing snake length
-    if direction == 'RIGHT' or direction == 'DOWN':
+    if direction == 'RIGHT':
         snakePosition[0] += 10
-    if direction == 'UP' or direction == 'LEFT':
+    if direction == 'UP':
+        snakePosition[1] -= 10
+    if direction == 'DOWN':
+        snakePosition[1] +=10
+    if direction == 'LEFT':
         snakePosition[0] -= 10
         
     #snake body mechanism
     snakeLength.insert(0, list(snakePosition))
+    
     if snakePosition[0] == foodPosition[0] and snakePosition[1] == foodPosition[1]:
+        score += 1
+        if score == 5:
+            speed = 25
+        if score == 10:
+            speed = 35
+            
         foodSpawn = False
     else:
         snakeLength.pop()
@@ -102,5 +127,20 @@ while True:
     #adding elements to the canvas(playing surface)
     
     playArea.fill(white)
+    for pos in snakeLength:
+        pygame.draw.rect(playArea, green, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(playArea, red, pygame.Rect(foodPosition[0], foodPosition[1], 10, 10))
+    
+    #adding boundary constraints
+    if snakePosition[0] > 790 or snakePosition[0] < 0:
+        gameOver()
+    elif snakePosition[1] > 590 or snakePosition[1] < 0:
+        gameOver()
+        
+    for block in snakeLength[1:]:
+        if snakePosition[0] == block[0] and snakePosition[1] == block[1]:
+            gameOver()
+    showScore()
     pygame.display.flip()
+    fpsController.tick(speed)
     
