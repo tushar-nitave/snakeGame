2af8687abd6e808cf02887f8b7b4cb34582c397a
tushar nitave <tushar nitave>
2017-09-27 13:23:06 +0530

    Name Plate

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+Name = "Tushar Nitave"
+email = "tushar.nitave@gmail.com"
+
+print("This is a simple snake game developed in python using pygame module.")
+print("Developed by:%s"%(Name))
+print("If you would like to contact me here is my email: %s"%(email))
